7fd16eb8965af7265b79f0dee9a959ec31b7d0f3
Ricardo Parro <ricardo.parro@gmail.com>
2015-10-06 22:00:49 +0200

    problem on active record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006193334) do
+ActiveRecord::Schema.define(version: 20151006195424) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151006193334) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "user_id_integer"
+    t.integer  "user_id"
   end
 
   add_index "links", ["user_id_integer"], name: "index_links_on_user_id_integer"
